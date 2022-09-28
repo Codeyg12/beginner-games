0a6048069a880db2218ae29894ffc43269febc79
Codeyg12 <codey.gallup@gmail.com>
2022-09-28 16:06:52 -0600

    added some comments

--- parallax-background/main.js
+++ parallax-background/main.js
@@ -1,72 +1,84 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 800;
-const CANVAS_HEIGHT = canvas.height = 700;
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 800);
+const CANVAS_HEIGHT = (canvas.height = 700);
 let gameSpeed = 5;
 
+// All layers of the background
 const backgroundLayer1 = new Image();
-backgroundLayer1.src = 'layer-1.png'
+backgroundLayer1.src = "layer-1.png";
 const backgroundLayer2 = new Image();
-backgroundLayer2.src = 'layer-2.png'
+backgroundLayer2.src = "layer-2.png";
 const backgroundLayer3 = new Image();
-backgroundLayer3.src = 'layer-3.png'
+backgroundLayer3.src = "layer-3.png";
 const backgroundLayer4 = new Image();
-backgroundLayer4.src = 'layer-4.png'
+backgroundLayer4.src = "layer-4.png";
 const backgroundLayer5 = new Image();
-backgroundLayer5.src = 'layer-5.png'
+backgroundLayer5.src = "layer-5.png";
 
-window.addEventListener('load', function() {
-    
-const slider = document.getElementById('slider');
-slider.value = gameSpeed;
-const showGameSpeed = document.getElementById('showGameSpeed');
-showGameSpeed.innerHTML = gameSpeed;
-slider.addEventListener('change', function(e) {
+// Starts the animation when the page has loaded
+window.addEventListener("load", function () {
+  // Slider so the user can change the speed of the background
+  const slider = document.getElementById("slider");
+  slider.value = gameSpeed;
+  const showGameSpeed = document.getElementById("showGameSpeed");
+  showGameSpeed.innerHTML = gameSpeed;
+  // On "change" the sliders value will be the new speed
+  slider.addEventListener("change", function (e) {
     gameSpeed = e.target.value;
     showGameSpeed.innerHTML = gameSpeed;
-})
+  });
 
-class Layer {
+  class Layer {
     constructor(image, speedModifier) {
-        this.x = 0;
-        this.y = 0;
-        this.width = 2400;
-        this.height = 700;
-        this.image = image;
-        this.speedModifier = speedModifier;
-        this.speed = gameSpeed * this.speedModifier;
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 700;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gameSpeed * this.speedModifier;
     }
 
     update() {
-        this.speed = gameSpeed * this.speedModifier;
-        if (this.x <= -this.width) {
-            this.x = 0;
-        }
-        this.x = this.x - this.speed
+      // Goes through the image and resets it after a full run
+      this.speed = gameSpeed * this.speedModifier;
+      if (this.x <= -this.width) {
+        this.x = 0;
+      }
+      this.x = this.x - this.speed;
     }
 
     draw() {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      ctx.drawImage(
+        this.image,
+        this.x + this.width,
+        this.y,
+        this.width,
+        this.height
+      );
     }
-}
+  }
 
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
-const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1);
+  // Every layer has a different image and moving speed
+  const layer1 = new Layer(backgroundLayer1, 0.2);
+  const layer2 = new Layer(backgroundLayer2, 0.4);
+  const layer3 = new Layer(backgroundLayer3, 0.6);
+  const layer4 = new Layer(backgroundLayer4, 0.8);
+  const layer5 = new Layer(backgroundLayer5, 1);
 
-const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+  // Creates gameObjects so it can go through the layers without repeating
+  const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
-function animate() {
+  function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    gameObjects.forEach(layer => {
-        layer.update();
-        layer.draw();
+    gameObjects.forEach((layer) => {
+      layer.update();
+      layer.draw();
     });
     requestAnimationFrame(animate);
-};
+  }
 
-animate();
+  animate();
 });
