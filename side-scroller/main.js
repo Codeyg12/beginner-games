0a9377f9ed1a70a1edf5af0cef8248028af93ebb
Codeyg12 <codey.gallup@gmail.com>
2022-09-29 10:31:46 -0600

    formatting

--- side-scroller/main.js
+++ side-scroller/main.js
@@ -1,316 +1,366 @@
-window.addEventListener('load', function() {
-// I don't call "getElementById" because the id has already made it a variable 
-const ctx = canvas.getContext('2d');
-canvas.width = 1300;
-canvas.height = 720;
-let enemies = [];
-let score = 0;
-let gameOver = false;
+window.addEventListener("load", function () {
+  // I don't call "getElementById" because the id has already made it a variable
+  const ctx = canvas.getContext("2d");
+  canvas.width = 1300;
+  canvas.height = 720;
+  let enemies = [];
+  let score = 0;
+  let gameOver = false;
 
-class InputHandler {
+  class InputHandler {
     constructor() {
-        this.keys = [];
-        this.touchY = '';
-        this.touchThreshold = 30;
-        // Keydown listener to see if an arrow key has been pressed and that it isnt in the array already 
-        window.addEventListener('keydown', e => {
-            if ((   e.key === 'ArrowDown' ||
-                    e.key === 'ArrowLeft' ||
-                    e.key === 'ArrowRight' ||
-                    e.key === 'ArrowUp') 
-                    && this.keys.indexOf(e.key) === -1) {
-                this.keys.push(e.key);
-            } else if (e.key === 'Enter' && gameOver) {
-                restartGame();
-            }
-        });
-        // Keyup listener to see when the arrow is released so it can be removed from the array
-        window.addEventListener('keyup', e => {
-            if (    e.key === 'ArrowDown' ||
-                    e.key === 'ArrowLeft' ||
-                    e.key === 'ArrowRight' || 
-                    e.key === 'ArrowUp') {
-                this.keys.splice(this.keys.indexOf(e.key), 1);
-            }
-        });
+      this.keys = [];
+      this.touchY = "";
+      this.touchThreshold = 30;
+      // Keydown listener to see if an arrow key has been pressed and that it isnt in the array already
+      window.addEventListener("keydown", (e) => {
+        if (
+          (e.key === "ArrowDown" ||
+            e.key === "ArrowLeft" ||
+            e.key === "ArrowRight" ||
+            e.key === "ArrowUp") &&
+          this.keys.indexOf(e.key) === -1
+        ) {
+          this.keys.push(e.key);
+        } else if (e.key === "Enter" && gameOver) {
+          restartGame();
+        }
+      });
+      // Keyup listener to see when the arrow is released so it can be removed from the array
+      window.addEventListener("keyup", (e) => {
+        if (
+          e.key === "ArrowDown" ||
+          e.key === "ArrowLeft" ||
+          e.key === "ArrowRight" ||
+          e.key === "ArrowUp"
+        ) {
+          this.keys.splice(this.keys.indexOf(e.key), 1);
+        }
+      });
 
-        window.addEventListener('touchstart', e => {
-            this.touchY = e.changedTouches[0].pageY
-        });
-        window.addEventListener('touchmove', e => {
-            const swipeDistance = e.changedTouches[0].pageY - this.touchY;
-            // Checks if the user has swiped more than 30 pixels and if so pushs the direction to the keys array
-            if (swipeDistance < -this.touchThreshold && this.keys.indexOf('swipe up') === -1) {
-                this.keys.push('swipe up');
-            } else if (swipeDistance > this.touchThreshold && this.keys.indexOf('swipe down') === -1) {
-                this.keys.push('swipe down');
-                // If the game is over swiping down will restart
-                if (gameOver) {
-                    restartGame();
-                }
-            }
-        });
-        window.addEventListener('touchend', e => {
-            // Splices the swipe direction away after its off the screen
-            this.keys.splice(this.keys.indexOf('swipe up'), 1);
-            this.keys.splice(this.keys.indexOf('swipe down'), 1);
-            this.keys.splice(this.keys.indexOf('swipe right'), 1);
-        });
+      window.addEventListener("touchstart", (e) => {
+        this.touchY = e.changedTouches[0].pageY;
+      });
+      window.addEventListener("touchmove", (e) => {
+        const swipeDistance = e.changedTouches[0].pageY - this.touchY;
+        // Checks if the user has swiped more than 30 pixels and if so pushs the direction to the keys array
+        if (
+          swipeDistance < -this.touchThreshold &&
+          this.keys.indexOf("swipe up") === -1
+        ) {
+          this.keys.push("swipe up");
+        } else if (
+          swipeDistance > this.touchThreshold &&
+          this.keys.indexOf("swipe down") === -1
+        ) {
+          this.keys.push("swipe down");
+          // If the game is over swiping down will restart
+          if (gameOver) {
+            restartGame();
+          }
+        }
+      });
+      window.addEventListener("touchend", (e) => {
+        // Splices the swipe direction away after its off the screen
+        this.keys.splice(this.keys.indexOf("swipe up"), 1);
+        this.keys.splice(this.keys.indexOf("swipe down"), 1);
+        this.keys.splice(this.keys.indexOf("swipe right"), 1);
+      });
     }
-}
+  }
 
-class Player {
+  class Player {
     constructor(gameWidth, gameHeight) {
-        this.gameWidth = gameWidth;
-        this.gameHeight = gameHeight;
-        this.width = 200;
-        this.height = 200;
-        this.x = 100;
-        // Starts the player on the ground 
-        this.y = this.gameHeight - this.height;
-        this.image = playerImage;
-        this.frameX = 0;
-        this.maxFrame = 8
-        this.frameY = 0;
-        this.fps = 20;
-        this.frameTimer = 0;
-        this.frameInterval = 1000/this.fps;
-        this.speed = .02;
-        this.vy = 0;
-        this.gravity = 1;
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 100;
+      // Starts the player on the ground
+      this.y = this.gameHeight - this.height;
+      this.image = playerImage;
+      this.frameX = 0;
+      this.maxFrame = 8;
+      this.frameY = 0;
+      this.fps = 20;
+      this.frameTimer = 0;
+      this.frameInterval = 1000 / this.fps;
+      this.speed = 0.02;
+      this.vy = 0;
+      this.gravity = 1;
     }
 
     reset() {
-        this.x = 100;
-        this.y = this.gameHeight - this.height;
-        this.maxFrame = 8
-        this.frameY = 0;
+      this.x = 100;
+      this.y = this.gameHeight - this.height;
+      this.maxFrame = 8;
+      this.frameY = 0;
     }
 
     draw(context) {
-        context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
     }
 
     update(input, deltaTime, enemies) {
-        // Collision detection
-        enemies.forEach(enemy => {
-            const dx = (enemy.x + enemy.width/2 - 10) - (this.x + this.width/2);
-            const dy = (enemy.y + enemy.height/2) - (this.y + this.height/2 + 20);
-            const distance = Math.sqrt(dx * dx + dy * dy);
-            if (distance < enemy.width/2.5 + this.width/2.5) {
-                gameOver = true;
-            }
-        })
-        // Controls the frames for running 
-        if (this.frameTimer > this.frameInterval) {
-            if (this.frameX >= this.maxFrame) {
-                this.frameX = 0;
-            } else {
-                this.frameX++;
-            }
-            this.frameTimer = 0
+      // Collision detection
+      enemies.forEach((enemy) => {
+        const dx = enemy.x + enemy.width / 2 - 10 - (this.x + this.width / 2);
+        const dy = enemy.y + enemy.height / 2 - (this.y + this.height / 2 + 20);
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < enemy.width / 2.5 + this.width / 2.5) {
+          gameOver = true;
+        }
+      });
+      // Controls the frames for running
+      if (this.frameTimer > this.frameInterval) {
+        if (this.frameX >= this.maxFrame) {
+          this.frameX = 0;
         } else {
-            this.frameTimer += deltaTime
+          this.frameX++;
         }
-        // Movement based on the keys pressed
-        if (input.keys.indexOf('ArrowRight') > -1 || input.keys.indexOf('swipe right') > -1) {
-            this.speed = 5;
-        } else if (input.keys.indexOf('ArrowLeft') > -1) {
-            this.speed = -5;
-        } else if ((input.keys.indexOf('ArrowUp') > -1 || input.keys.indexOf('swipe up') > -1) && this.onGround()) {
-            this.vy -= 30;
-        } else {
-            this.speed = 0;
-        }
+        this.frameTimer = 0;
+      } else {
+        this.frameTimer += deltaTime;
+      }
+      // Movement based on the keys pressed
+      if (
+        input.keys.indexOf("ArrowRight") > -1 ||
+        input.keys.indexOf("swipe right") > -1
+      ) {
+        this.speed = 5;
+      } else if (input.keys.indexOf("ArrowLeft") > -1) {
+        this.speed = -5;
+      } else if (
+        (input.keys.indexOf("ArrowUp") > -1 ||
+          input.keys.indexOf("swipe up") > -1) &&
+        this.onGround()
+      ) {
+        this.vy -= 30;
+      } else {
+        this.speed = 0;
+      }
 
-        // Horizontal
-        this.x += this.speed;
-        // Stops the character from going off screen left or right, x axis
-        if (this.x < 0) {
-            this.x = 0;
-        } else if (this.x > this.gameWidth - this.width) {
-            this.x = this.gameWidth - this.width
-        }
-
-        // Vertical
-        this.y += this.vy;
-        // If the player isn't on the ground "gravity" will take effect, also changes the player to "jumping" frame
-        if (!this.onGround()) {
-            this.vy += this.gravity;
-            this.maxFrame = 5;
-            this.frameY = 1;
-        } else {
-            this.vy = 0;
-            this.maxFrame = 8;
-            this.frameY = 0;
-        }
-        // Stops the player from falling through the ground after a jump
-        if (this.y > this.gameHeight - this.height) {
-            this.y = this.gameHeight - this.height
-        }
-    }
-
-    // Checks if the player is on the ground 
-    onGround() {
-        return this.y >= this.gameHeight - this.height;
-    }
-}
-
-class Background {
-    constructor(gameWidth, gameHeight) {
-        this.gameWidth = gameWidth;
-        this.gameHeight = gameHeight;
-        this.image = backgroundImage;
+      // Horizontal
+      this.x += this.speed;
+      // Stops the character from going off screen left or right, x axis
+      if (this.x < 0) {
         this.x = 0;
-        this.y = 0;
-        this.width = 2400;
-        this.height = 720;
-        this.speed = 7;
+      } else if (this.x > this.gameWidth - this.width) {
+        this.x = this.gameWidth - this.width;
+      }
+
+      // Vertical
+      this.y += this.vy;
+      // If the player isn't on the ground "gravity" will take effect, also changes the player to "jumping" frame
+      if (!this.onGround()) {
+        this.vy += this.gravity;
+        this.maxFrame = 5;
+        this.frameY = 1;
+      } else {
+        this.vy = 0;
+        this.maxFrame = 8;
+        this.frameY = 0;
+      }
+      // Stops the player from falling through the ground after a jump
+      if (this.y > this.gameHeight - this.height) {
+        this.y = this.gameHeight - this.height;
+      }
+    }
+
+    // Checks if the player is on the ground
+    onGround() {
+      return this.y >= this.gameHeight - this.height;
+    }
+  }
+
+  class Background {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.image = backgroundImage;
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 720;
+      this.speed = 7;
     }
 
     reset() {
-        this.x = 0;
+      this.x = 0;
     }
 
     draw(context) {
-        // Doubles the background so it seems endless
-        context.drawImage(this.image, this.x, this.y, this.width, this.height)
-        context.drawImage(this.image, this.x + this.width - this.speed, this.y, this.width, this.height)
+      // Doubles the background so it seems endless
+      context.drawImage(this.image, this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.x + this.width - this.speed,
+        this.y,
+        this.width,
+        this.height
+      );
     }
 
     update() {
-        this.x -= this.speed;
-        if (this.x < 0 - this.width) {
-            this.x = 0;
-        }
+      this.x -= this.speed;
+      if (this.x < 0 - this.width) {
+        this.x = 0;
+      }
     }
-}
+  }
 
-class Enemy {
+  class Enemy {
     constructor(gameWidth, gameHeight) {
-        this.gameWidth = gameWidth;
-        this.gameHeight = gameHeight;
-        this.width = 160;
-        this.height = 119;
-        this.image = enemyImage;
-        this.x = this.gameWidth;
-        this.y = this.gameHeight - this.height;
-        this.frameX = 0;
-        this.maxFrame = 5;
-        this.fps = 20;
-        this.frameTimer = 0;
-        this.frameInterval = 1000/this.fps;
-        this.speed = 8;
-        this.markedForDeletion = false;
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 160;
+      this.height = 119;
+      this.image = enemyImage;
+      this.x = this.gameWidth;
+      this.y = this.gameHeight - this.height;
+      this.frameX = 0;
+      this.maxFrame = 5;
+      this.fps = 20;
+      this.frameTimer = 0;
+      this.frameInterval = 1000 / this.fps;
+      this.speed = 8;
+      this.markedForDeletion = false;
     }
 
     draw(context) {
-        context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        0,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
     }
 
     update(deltaTime) {
-        // If the frame its currently on is GTE the max frames reset otherwise move up a frame
-        if (this.frameTimer > this.frameInterval) {
-          if (this.frameX >= this.maxFrame) {
-            this.frameX = 0;
-          } else {
-            this.frameX++
-          }
-          this.frameTimer = 0;
+      // If the frame its currently on is GTE the max frames reset otherwise move up a frame
+      if (this.frameTimer > this.frameInterval) {
+        if (this.frameX >= this.maxFrame) {
+          this.frameX = 0;
         } else {
-            this.frameTimer += deltaTime
+          this.frameX++;
         }
-        this.x -= this.speed;
-        // Marks the enemies for deletion after they've crossed the screen
-        if (this.x < 0 - this.width) {
-            this.markedForDeletion = true;
-            score++;
-        }
+        this.frameTimer = 0;
+      } else {
+        this.frameTimer += deltaTime;
+      }
+      this.x -= this.speed;
+      // Marks the enemies for deletion after they've crossed the screen
+      if (this.x < 0 - this.width) {
+        this.markedForDeletion = true;
+        score++;
+      }
     }
-}
+  }
 
-function handleEnemies(deltaTime) {
+  function handleEnemies(deltaTime) {
     // Checks when to push a new enemy onto the screen/into the array
     if (enemyTimer > enemyIntreval + randomEnemyInterval) {
-        enemies.push(new Enemy(canvas.width, canvas.height));
-        randomEnemyInterval = Math.random() * 1000 + 500
-        enemyTimer = 0;
+      enemies.push(new Enemy(canvas.width, canvas.height));
+      randomEnemyInterval = Math.random() * 1000 + 500;
+      enemyTimer = 0;
     } else {
-        enemyTimer += deltaTime;
+      enemyTimer += deltaTime;
     }
     // Draws and updates for each enemy
-    enemies.forEach(enemy => {
-        enemy.draw(ctx);
-        enemy.update(deltaTime);
-    })
+    enemies.forEach((enemy) => {
+      enemy.draw(ctx);
+      enemy.update(deltaTime);
+    });
 
     //filters out the marked for deletion enemies
-    enemies = enemies.filter(enemy => !enemy.markedForDeletion);
+    enemies = enemies.filter((enemy) => !enemy.markedForDeletion);
+  }
 
-}
-
-function displayStatusText(context) {
+  function displayStatusText(context) {
     // Scoreboard
-    context.fillStyle = 'black';
-    context.font = '40px Helvetica';
-    context.fillText('Score: ' + score, 20, 50)
-    context.fillStyle = 'white';
-    context.fillText('Score: ' + score, 22, 48)
+    context.fillStyle = "black";
+    context.font = "40px Helvetica";
+    context.fillText("Score: " + score, 20, 50);
+    context.fillStyle = "white";
+    context.fillText("Score: " + score, 22, 48);
 
     // If gameover tells how to restart
     if (gameOver) {
-        context.textAlign = 'center';
-        context.fillStyle = 'black';
-        context.fillText('GAME OVER! To play again press enter or swipedown', canvas.width/2, 200)
-        context.fillStyle = 'white';
-        context.fillText('GAME OVER! To play again press enter or swipedown', canvas.width/2 + 2, 198)
+      context.textAlign = "center";
+      context.fillStyle = "black";
+      context.fillText(
+        "GAME OVER! To play again press enter or swipedown",
+        canvas.width / 2,
+        200
+      );
+      context.fillStyle = "white";
+      context.fillText(
+        "GAME OVER! To play again press enter or swipedown",
+        canvas.width / 2 + 2,
+        198
+      );
     }
-}
+  }
 
-// Restarts the game by setting everything back to original state
-function restartGame() {
+  // Restarts the game by setting everything back to original state
+  function restartGame() {
     player.reset();
     background.reset();
     enemies = [];
     score = 0;
     gameOver = false;
-    animate(0)
-}
+    animate(0);
+  }
 
-function toggleFullscreen() {
+  function toggleFullscreen() {
     if (!document.fullscreenElement) {
-        canvas.requestFullscreen().catch(err => {
-            alert(`Error, can't enable fullscreen: ${err.message}`)
-        });
+      canvas.requestFullscreen().catch((err) => {
+        alert(`Error, can't enable fullscreen: ${err.message}`);
+      });
     } else {
-        document.exitFullscreen();
+      document.exitFullscreen();
     }
-}
+  }
 
-fullscreenBtn.addEventListener('click', toggleFullscreen)
+  fullscreenBtn.addEventListener("click", toggleFullscreen);
 
-const input = new InputHandler();
-const player = new Player(canvas.width, canvas.height);
-const background = new Background(canvas.width, canvas.height);
+  const input = new InputHandler();
+  const player = new Player(canvas.width, canvas.height);
+  const background = new Background(canvas.width, canvas.height);
 
-let lastTime = 0;
-let enemyTimer = 0;
-let enemyIntreval = 1000;
-let randomEnemyInterval = Math.random() * 1000 + 500;
+  let lastTime = 0;
+  let enemyTimer = 0;
+  let enemyIntreval = 1000;
+  let randomEnemyInterval = Math.random() * 1000 + 500;
 
-function animate(timestamp) {
+  function animate(timestamp) {
     const deltaTime = timestamp - lastTime;
     lastTime = timestamp;
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    background.draw(ctx)
-    background.update()
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    background.draw(ctx);
+    background.update();
     player.draw(ctx);
     player.update(input, deltaTime, enemies);
-    handleEnemies(deltaTime)
-    displayStatusText(ctx)
+    handleEnemies(deltaTime);
+    displayStatusText(ctx);
     if (!gameOver) {
-        requestAnimationFrame(animate)
+      requestAnimationFrame(animate);
     }
-}
-animate(0);
+  }
+  animate(0);
 });
