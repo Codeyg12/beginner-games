c20396800cbb56c0abf029c687e9f17d3956d84a
Codeyg12 <codey.gallup@gmail.com>
2022-09-30 20:36:23 -0600

    locked player to canvas size, stopped diagonal movement, updated food class

--- snake/main.js
+++ snake/main.js
@@ -1,6 +1,7 @@
 const ctx = canvas.getContext("2d");
 canvas.width = 700;
 canvas.height = 700;
+let gameOver = false;
 
 class InputHandler {
   constructor() {
@@ -14,10 +15,10 @@ class InputHandler {
         this.keys.indexOf(e.key) === -1
       ) {
         this.keys.push(e.key);
-        console.log(this.keys)
-      } // else if (e.key === "Enter" && gameOver) {
-      //     restartGame();
-      //   }
+        console.log(this.keys);
+      } else if (e.key === "Enter" && gameOver) {
+        restartGame();
+      }
     });
 
     window.addEventListener("keyup", (e) => {
@@ -28,7 +29,7 @@ class InputHandler {
         e.key === "ArrowUp"
       ) {
         this.keys.splice(this.keys.indexOf(e.key), 1);
-        console.log(this.keys)
+        console.log(this.keys);
       }
     });
   }
@@ -38,8 +39,8 @@ class Player {
   constructor(gameWidth, gameHeight) {
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
-    this.x = 0;
-    this.y = 0;
+    this.x = 1;
+    this.y = 1;
     this.width = 20;
     this.height = 20;
     this.speedX = 0;
@@ -49,16 +50,16 @@ class Player {
   update(input) {
     if (input.keys.indexOf("ArrowRight") > -1) {
       this.speedX = 3;
-      this.speedY = 0
+      this.speedY = 0;
     } else if (input.keys.indexOf("ArrowLeft") > -1) {
       this.speedX = -3;
-      this.speedY = 0
+      this.speedY = 0;
     } else if (input.keys.indexOf("ArrowUp") > -1) {
       this.speedY = -3;
-      this.speedX = 0
+      this.speedX = 0;
     } else if (input.keys.indexOf("ArrowDown") > -1) {
       this.speedY = 3;
-      this.speedX = 0
+      this.speedX = 0;
     }
 
     // Horizontal
@@ -76,44 +77,81 @@ class Player {
     } else if (this.y > this.gameHeight - this.height) {
       this.y = this.gameHeight - this.height;
     }
+
+    // Collision detection
+    if (
+      this.x >= this.gameWidth - this.width ||
+      this.x <= 0 ||
+      this.y >= this.gameHeight - this.height ||
+      this.y <= 0
+    ) {
+      gameOver = true;
+    }
   }
 
   draw(context) {
     context.fillStyle = "white";
     context.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  reset() {
+    this.x = 1;
+    this.y = 1;
   }
 }
 
-class Enemy {
+class Food {
   constructor(gameWidth, gameHeight) {
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
-    this.x = Math.floor(Math.random() * gameWidth);
-    this.y = Math.floor(Math.random() * gameHeight);
     this.width = 20;
     this.height = 20;
+    this.x = Math.floor(Math.random() * this.gameWidth - this.width);
+    this.y = Math.floor(Math.random() * this.gameHeight - this.height);
   }
 
-  update() {
-
-  }
+  update() {}
 
   draw(context) {
-    context.fillStyle = "white";
+    context.fillStyle = "red";
     context.fillRect(this.x, this.y, this.width, this.height);
   }
 
+  reset() {
+    this.x = Math.floor(Math.random() * this.gameWidth - this.width);
+    this.y = Math.floor(Math.random() * this.gameHeight - this.height);
+  }
+
+}
+
+function gameStatus(context) {
+  if (gameOver) {
+    context.textAlign = "center";
+    context.font = "40px Helvetica";
+    context.fillStyle = "white";
+    context.fillText("GAME OVER!", canvas.width / 2, 200);
+  }
+}
+
+function restartGame() {
+  player.reset();
+  food.reset();
+  gameOver = false;
+  animate(0);
 }
 
 const input = new InputHandler();
 const player = new Player(canvas.width, canvas.height);
-const enemy = new Enemy(canvas.width, canvas.height)
+const food = new Food(canvas.width, canvas.height);
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  requestAnimationFrame(animate);
   player.draw(ctx);
   player.update(input);
-  enemy.draw(ctx)
+  food.draw(ctx);
+  gameStatus(ctx);
+  if (!gameOver) {
+    requestAnimationFrame(animate);
+  }
 }
-animate();
+animate(0);
