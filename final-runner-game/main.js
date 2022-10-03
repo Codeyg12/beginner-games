eae61e9cd1653a4412c3878b75c85566316edb91
Codeyg12 <codey.gallup@gmail.com>
2022-10-03 11:18:19 -0600

    added enemies

--- final-runner-game/main.js
+++ final-runner-game/main.js
@@ -1,6 +1,7 @@
 import { Player } from './player.js';
 import { InputHandler } from './input.js'
 import { Background } from './background.js'
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from './enemies.js'
 
 window.addEventListener('load', function() {
 const ctx = canvas.getContext('2d');
@@ -12,21 +13,52 @@ class Game {
         this.width = width;
         this.height = height;
         this.groundMargin = 80;
-        this.speed = 3;
+        this.speed = 0;
         this.maxSpeed = 3;
         this.background = new Background(this)
         this.player = new Player(this);
         this.input = new InputHandler();
+        this.enemies = [];
+        this.enemyTimer = 0;
+        this.enemyInterval = 1000;
     }
 
     update(deltaTime) {
         this.background.update();
         this.player.update(this.input.keys, deltaTime);
+
+        // Handle enemies
+        if (this.enemyTimer > this.enemyInterval) {
+            this.addEnemy();
+            this.enemyTimer = 0;
+        } else {
+            this.enemyTimer += deltaTime
+        }
+
+        // Triggers update for eneimers
+        this.enemies.forEach(enemy => {
+            enemy.update(deltaTime);
+            if (enemy.markedForDeletion) {
+                this.enemies.splice(this.enemies.indexOf(enemy), 1);
+            }
+        })
     }
 
     draw(context) {
         this.background.draw(context);
         this.player.draw(context);
+        this.enemies.forEach(enemy => {
+            enemy.draw(context);
+        })
+    }
+
+    addEnemy() {
+        if (this.speed > 0 && Math.random() < 0.5) {
+            this.enemies.push(new GroundEnemy(this));
+        } else if (this.speed > 0) {
+            this.enemies.push(new ClimbingEnemy(this));
+        }
+        this.enemies.push(new FlyingEnemy(this))
     }
 }
 
