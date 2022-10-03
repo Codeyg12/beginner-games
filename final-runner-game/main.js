243c52223c2f59df11e208ade293e83059530f15
Codeyg12 <codey.gallup@gmail.com>
2022-10-03 10:27:13 -0600

    added assets folder

--- final-runner-game/main.js
+++ final-runner-game/main.js
@@ -1,5 +1,6 @@
 import { Player } from './player.js';
 import { InputHandler } from './input.js'
+import { Background } from './background.js'
 
 window.addEventListener('load', function() {
 const ctx = canvas.getContext('2d');
@@ -10,28 +11,37 @@ class Game {
     constructor(width, height) {
         this.width = width;
         this.height = height;
+        this.groundMargin = 80;
+        this.speed = 3;
+        this.maxSpeed = 3;
+        this.background = new Background(this)
         this.player = new Player(this);
         this.input = new InputHandler();
     }
 
-    update() {
-        this.player.update(this.input.keys);
+    update(deltaTime) {
+        this.background.update();
+        this.player.update(this.input.keys, deltaTime);
     }
 
     draw(context) {
+        this.background.draw(context);
         this.player.draw(context);
     }
 }
 
 const game = new Game(canvas.width, canvas.height);
 
-function animate() {
+let lastTime = 0;
+function animate(timestamp) {
+    const deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    game.update();
+    game.update(deltaTime);
     game.draw(ctx);
     requestAnimationFrame(animate);
 }
 
-animate()
+animate(0)
 
 });
