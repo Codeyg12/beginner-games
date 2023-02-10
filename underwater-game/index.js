d8a7b91b93a9bdad0200ffbbea6548d3c348ada4
Codeyg12 <codey.gallup@gmail.com>
2023-02-09 19:03:34 -0700

    added input and player movement

--- underwater-game/index.js
+++ underwater-game/index.js
@@ -5,8 +5,20 @@ window.addEventListener('load', () => {
     canvas.height = 500
 
     class InputHandler {
-        constructor() {
-
+        constructor(game) {
+            this.game = game;
+            window.addEventListener('keydown', (e) => {
+                if ((e.key === "ArrowUp" || e.key === "ArrowDown") && !this.game.keys.includes(e.key)) {
+                    this.game.keys.push(e.key)
+                }
+                console.log(this.game.keys)
+            })
+            window.addEventListener('keyup', (e) => {
+                if (this.game.keys.includes(e.key)) {
+                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1)
+                }
+                console.log(this.game.keys)
+            })
         }
 
     }
@@ -28,8 +40,16 @@ window.addEventListener('load', () => {
             this.x = 20;
             this.y = 100;
             this.speedY = 0;
+            this.maxSpeed = 2;
         }
         update() {
+            if (this.game.keys.includes('ArrowUp')) {
+                this.speedY = -this.maxSpeed
+            } else if (this.game.keys.includes('ArrowDown')) {
+                this.speedY = this.maxSpeed
+            } else {
+                this.speedY = 0;
+            }
             this.y += this.speedY
         }
         draw(context) {
@@ -61,6 +81,8 @@ window.addEventListener('load', () => {
             this.width = width;
             this.height = height;
             this.player = new Player(this);
+            this.input = new InputHandler(this);
+            this.keys = [];
         }
         update() {
             this.player.update();
